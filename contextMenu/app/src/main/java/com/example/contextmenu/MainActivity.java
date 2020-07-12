a7e99b76f5edc91ca10f6e4618a50d69ed5cdecf
sindhu-sankati <sindhu_sankati@srmap.edu.in>
2020-07-12 15:42:56 +0530

    updated: task7 context menu

--- contextMenu/app/src/main/java/com/example/contextmenu/MainActivity.java
+++ contextMenu/app/src/main/java/com/example/contextmenu/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.contextmenu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ActionMode;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button b1;
+    private ActionMode mActionMode;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1 = findViewById(R.id.button);
+
+        registerForContextMenu(b1);
+    }
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+    }
+
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.option1:
+                Toast.makeText(this, "Clicked on Upload", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.option2:
+                Toast.makeText(this, "Clicked on Search", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.option3:
+                Toast.makeText(this, "Clicked on Share", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.option4:
+                Toast.makeText(this, "Clicked on Bookmark", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return false;
+        }
     }
 }
